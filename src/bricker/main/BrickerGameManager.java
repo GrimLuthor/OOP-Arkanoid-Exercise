f72ff62e7135b2ef0854633e6ad576db44648412
GrimLuthor <daniel.olenev@gmail.com>
2024-02-01 18:32:25 +0200

    test

--- src/bricker/main/BrickerGameManager.java
+++ src/bricker/main/BrickerGameManager.java
@@ -5,7 +5,9 @@ import danogl.util.Vector2;
 
 public class BrickerGameManager {
     public static void main(String[] args) {
-        GameManager gameManager =  new GameManager("Bricker",new Vector2(700,500));
-        gameManager.run();
+//        GameManager gameManager =  new GameManager("Bricker",new Vector2(700,500));
+//        gameManager.run();
+
+        System.out.println("Hello World!");
     }
 }
